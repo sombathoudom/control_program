28887065eaa98b837f88369b966bf24d51503c11
soklay24 <soklay@mango-byte.com>
2023-01-28 15:16:17 +0700

    add sample page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>{{ count }}</div>
+    <v-btn @click="onClick">Click</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+
+function onClick() {
+  count.value++;
+  console.log("clicked");
+}
+</script>
