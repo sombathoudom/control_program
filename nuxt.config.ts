4f39e3e794c0c8fb13e33c1aeccff639a480bba6
soklay24 <soklay@mango-byte.com>
2023-01-28 15:02:30 +0700

    remove vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,6 @@ export default defineNuxtConfig({
     modules: [
         '@nuxt-alt/proxy',
     ],
-    css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
-    build: {
-        transpile: ['vuetify'],
-    },
-    vite: {
-        define: {
-            'process.env.DEBUG': false,
-        },
-    },
     proxy: {
         
     }
